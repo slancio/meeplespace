6c1c90f18ce13b776dab3f6d12c932e731bfe6e2
Eric Savadian <eric.j.savadian@gmail.com>
2015-05-26 21:03:36 -0400

    Show basic information on page in event show view

--- app/assets/javascripts/views/events/show.js
+++ app/assets/javascripts/views/events/show.js
@@ -1,14 +1,31 @@
 Meeplespace.Views.EventShow = Backbone.CompositeView.extend({
   
   initialize: function (options) {
+    this._attendees = this.model.attendees();
+    this._eventHost = this.model.eventHost();
+
+    this.listenTo(this._attendees, "add", this.addAttendeeView);
+    this._attendees.each(this.addAttendeeView.bind(this));
+
     var that = this;
     this.listenTo(this.model, "sync", function () {
-      that.initMap();
+      that.initMap({ location: this.model.escape('location') });
       that.render();
+      that.addHostView(that._eventHost);
     });
   },
 
   template: JST['events/show'],
+
+  addAttendeeView: function (attendee) {
+    this._attendeeView = new Meeplespace.Views.AttendeeLink({ model: attendee });
+    this.addSubview('.attendees', this._attendeeView);
+  },
+
+  addHostView: function (host) {
+    this._hostView = new Meeplespace.Views.HostLink({ model: host });
+    this.addSubview('.host', this._hostView);
+  },
 
   initMap: function () {
     this._gMap = new Meeplespace.Views.ShowMap({
